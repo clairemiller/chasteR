57e6e4504aebc7187c6eb8ef5646b3a0ba99f046
Claire Miller <claire.miller@auckland.ac.nz>
2023-08-04 14:01:47 +1200

    General update

--- R/dataManipulation.R
+++ R/dataManipulation.R
@@ -23,3 +23,101 @@ getCellCounts <- function(data,column)
   d$time <- as.numeric(gsub("TimeStep_","",d$.id))
   return( d )
 }
+
+#' Approximate connectivity by voronoi tesselation in 2d
+#' @param x the x locations
+#' @param y the y locations
+#' @param width the periodic width
+#' @param b_dist the boundary distance
+#' @export
+getVoronoiConnectivity2d <- function(x,y,width,b_dist=2.0)
+{
+  ghost_nodes = list( x=c(x[x<=b_dist]+width,x[x >= (width-b_dist)]-width),
+                      y=c(y[x<=b_dist],y[x >= (width-b_dist)]) )
+  tess_data <- deldir(x,y,dpl=ghost_nodes)
+  output <- list(connectivity = tess_data[["summary"]][tess_data[["summary"]]$pt.type=="data",c("x","y","n.tside")],
+                 voronoi_lines = tess_data[["dirsgs"]][,c("x1","y1","x2","y2")],
+                 neighbours = tess_data[["delsgs"]],
+                 ghost_nodes=do.call(cbind,ghost_nodes))
+  return(output)
+}
+
+
+#' Approximate connectivity by voronoi tesselation
+#' @param x the x locations
+#' @param y the y locations
+#' @param z the z locations
+#' @param width the periodic width
+#' @param b_dist the boundary distance
+#' @export
+getVoronoiConnectivity3d <- function(x,y,z,width,b_dist=2.0)
+{
+  # Create an appropriate matrix
+  d <- as.matrix( cbind(x,y,z) )
+  output <- delaunayn(d)
+  return(output)
+}
+
+#' Convert a named vector from time steps to data frame with days
+#' @param v the named vector
+#' @param value_name the string part of the names to gsub out
+#' @export
+convertNamedTimeStepVector <- function(v, value_name="v")
+{
+  d <- data.frame(Day=as.numeric(gsub("TimeStep_","",names(v)))/24)
+  d[,value_name] <- as.vector(v)
+  return(d)
+}
+
+#' Load in a results.vizancestors file
+#' Reading in of the cell ancestory output
+#' @param filepath the path to the containing results folder
+#' @export
+getCellAncestory<-function(filepath)
+{
+  # Read in ancestors and determines the colonies
+  ancestors<-readChasteResultsFile(paste(filepath,"results.vizancestors",sep="/"))
+  n_colonies <- lapply(ancestors,table)
+  n_colonies <- melt(n_colonies)
+
+  # Tidy up the dataset
+  colnames(n_colonies) <- c("Ancestor","NumberCells","TimeStep")
+  n_colonies$Ancestor = factor(paste("Cell",n_colonies$Ancestor))#,levels=paste("Cell",as.character(5:0)))
+  n_colonies$TimeStep = gsub("TimeStep_","",n_colonies$TimeStep)
+  n_colonies$Day = as.numeric(n_colonies$TimeStep)/24
+
+  # Get the number of colonies over time
+  counts_colonies <- lapply(ancestors,unique)
+  counts_colonies <- sapply(counts_colonies,nrow)
+  counts_colonies <- data.frame( Day=as.numeric(gsub("TimeStep_","",names(counts_colonies)))/24, Colonies=as.numeric(counts_colonies)  )
+
+  return(list(raw=ancestors,colony_size=n_colonies,num_colonies=counts_colonies))
+}
+
+#' Generate the hybrid force functions given peak, location of peak, and cutoff
+#' @param x the x locations
+#' @param max_F the peak force
+#' @param peak_location the peak location
+#' @export
+calcHybridForce<-function(
+    x = seq(-0.3,2.5,0.01),
+    max_F = 0.2, peak_location = 1.1)
+{
+  alpha = 1/peak_location
+  y <- data.frame(x,force=0,fn="Repulsion")
+  # Calculate repulsion
+  spring_stiffness = max_F * alpha * exp(1.0)
+  y$force[x < 0] = spring_stiffness * log(1.0 + x[x < 0])
+  # Calculate adhesion/attraction
+  y$force[x > 0] = max_F * alpha * x[x>0] * exp(1.0 - alpha * x[x>0])
+  # Add an extra 0 row for visualisation purposes
+  y$fn[x > 0] = "Adhesion"
+  if (sum(x==0) > 0) {
+    y <- rbind(y,data.frame(x=0,force=0,fn="Adhesion"))
+  }
+  y <- y[order(y$x),]
+  return(y)
+}
+
+
+
--- R/readFunctions.R
+++ R/readFunctions.R
@@ -8,26 +8,24 @@
 #' @export
 readChasteResultsFile <- function(filename, filepath=".", columns="v")
 {
-  # Initial less efficient read method:
-  # con <- file(paste(filepath,filename,sep="/"),"r")
-  # dat <- readLines(con)
-  # close(con)
-  
   # New read method using readChar:
-  fullfilename <- paste(filepath,filename,sep="/")
+  fullfilename <- file.path(filepath,filename)
   size <- file.info(fullfilename)$size
   dat <- readChar(fullfilename,size,useBytes=T)
   dat <- unlist(strsplit(dat,"\r\n|\n|\r"))
-  
+
   # Split the string into the desired columns
   dat <- strsplit(dat,"\\s+")
+  # Remove any empty rows
+  i <- which(sapply(dat,length) > 1)
+  dat <- dat[i]
   dat <- lapply(dat, function(x) {
     t <- as.numeric(x[1]) # Extract the time
     d <- as.numeric(x[-1]) # Convert data to numeric
     d <- matrix(d,ncol=length(columns),byrow=TRUE,dimnames = list(rows=c(),columns))
     return( data.frame(time=t,d) )
   })
-  dat <- rbindlist(dat,use.names=T)
+  dat <- do.call(rbind,dat)
   return(dat)
 }
 
@@ -38,7 +36,7 @@ readChasteResultsFile <- function(filename, filepath=".", columns="v")
 #' @param num_dims the number of dimensions of the simulation
 #' @return A list of data frames, one for each time step, containing the cell locations.
 #' @export
-readCellLocations<-function(filepath,num_dims=2)
+readCellLocations<-function(filepath,num_dims=3)
 {
   columns <- c("x","y","z")[1:num_dims]
   cell_locations <- readChasteResultsFile("results.viznodes",filepath=filepath,columns=columns)
@@ -47,12 +45,12 @@ readCellLocations<-function(filepath,num_dims=2)
 
 
 #' Read a cell data file
-#' 
+#'
 #' Loads in data from a cell data output
 #' @param var_name the variable name of the cell data from chaste
 #' @return A data frame containing the cell data output
 #' @export
-readCellData<-function(filepath,var_name, dimensions = 2)
+readCellData<-function(filepath,var_name, dimensions = 3)
 {
   filename <- paste0("celldata_",var_name,".dat")
   dim_names <- c("x","y","z")[1:dimensions]
@@ -60,3 +58,17 @@ readCellData<-function(filepath,var_name, dimensions = 2)
   return(readChasteResultsFile(filepath=filepath,filename=filename,columns=columns))
 }
 
+#' Read a cell velocities file
+#'
+#' Loads in data from NodeVelocityWriter output
+#' Note, has got my modification where the cell id is not repeated
+#' @param filepath the folder path to where the simulation output is located
+#' @param dimensions the number of dimensions of the simulation
+#' @export
+readCellVelocities<-function(filepath, dimensions = 3)
+{
+  filename <- "nodevelocities.dat"
+  dim_seq <- 1:dimensions
+  columns <- c("id",c("x","y","z")[dim_seq],c("vx","vy","vz")[dim_seq])
+  return(readChasteResultsFile(filepath=filepath,filename=filename,columns=columns))
+}
